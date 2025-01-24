095abc541cc8add6d2d523c6c60dbda978f2de66
Avishay Zarad <zarad.avishay@gmail.com>
2025-01-24 16:54:37 +0100

    Physics part 1

---
+++ lessons/20 - Physics/src/script.js
@@ -0,0 +1,213 @@
+import CANNON from "cannon";
+import GUI from "lil-gui";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { depth } from "three/examples/jsm/nodes/Nodes.js";
+
+/**
+ * Debug
+ */
+const gui = new GUI();
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader();
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+
+const environmentMapTexture = cubeTextureLoader.load([
+	"/textures/environmentMaps/0/px.png",
+	"/textures/environmentMaps/0/nx.png",
+	"/textures/environmentMaps/0/py.png",
+	"/textures/environmentMaps/0/ny.png",
+	"/textures/environmentMaps/0/pz.png",
+	"/textures/environmentMaps/0/nz.png",
+]);
+
+//PHYSICS
+const world = new CANNON.World();
+world.gravity.set(0, -9.82, 0);
+
+// 1. we can create different materials, and give them to different bodies
+// const concreteMaterial = new CANNON.Material("concrete");
+// const plasticMaterial = new CANNON.Material("plastic");
+// const concretePlasticContactMaterial = new CANNON.ContactMaterial(
+// 	concreteMaterial,
+// 	plasticMaterial,
+// 	{
+// 		friction: 0.1,
+// 		restitution: 0.7,
+// 	},
+// );
+// world.addContactMaterial(concretePlasticContactMaterial);
+
+// 2. or we can create a default material and give it to all bodies
+const defaultMaterial = new CANNON.Material("default");
+const defaultContactMaterial = new CANNON.ContactMaterial(
+	defaultMaterial,
+	defaultMaterial,
+	{
+		friction: 0.1,
+		restitution: 0.7,
+	},
+);
+
+const sphereShape = new CANNON.Sphere(0.5);
+const sphereBody = new CANNON.Body({
+	mass: 1,
+	position: new CANNON.Vec3(0, 3, 0),
+	shape: sphereShape,
+	material: defaultContactMaterial,
+});
+world.addBody(sphereBody);
+
+sphereBody.applyLocalForce(
+	new CANNON.Vec3(150, 0, 0),
+	new CANNON.Vec3(0, 0, 0),
+);
+
+// 3. we can set this as the default contact material for all bodies in the world
+// world.defaultContactMaterial = defaultContactMaterial;
+
+const floorShape = new CANNON.Plane();
+const floorBody = new CANNON.Body({
+	mass: 0,
+	position: new CANNON.Vec3(0, 0, 0),
+	shape: floorShape,
+	material: defaultContactMaterial,
+});
+
+floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI * 0.5);
+world.addBody(floorBody);
+
+/**
+ * Test sphere
+ */
+const sphere = new THREE.Mesh(
+	new THREE.SphereGeometry(0.5, 32, 32),
+	new THREE.MeshStandardMaterial({
+		metalness: 0.3,
+		roughness: 0.4,
+		envMap: environmentMapTexture,
+		envMapIntensity: 0.5,
+	}),
+);
+sphere.castShadow = true;
+sphere.position.y = 0.5;
+scene.add(sphere);
+
+/**
+ * Floor
+ */
+const floor = new THREE.Mesh(
+	new THREE.PlaneGeometry(10, 10),
+	new THREE.MeshStandardMaterial({
+		color: "#777777",
+		metalness: 0.3,
+		roughness: 0.4,
+		envMap: environmentMapTexture,
+		envMapIntensity: 0.5,
+	}),
+);
+floor.receiveShadow = true;
+floor.rotation.x = -Math.PI * 0.5;
+scene.add(floor);
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 2.1);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.set(1024, 1024);
+directionalLight.shadow.camera.far = 15;
+directionalLight.shadow.camera.left = -7;
+directionalLight.shadow.camera.top = 7;
+directionalLight.shadow.camera.right = 7;
+directionalLight.shadow.camera.bottom = -7;
+directionalLight.position.set(5, 5, 5);
+scene.add(directionalLight);
+
+/**
+ * Sizes
+ */
+const sizes = {
+	width: window.innerWidth,
+	height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+	// Update sizes
+	sizes.width = window.innerWidth;
+	sizes.height = window.innerHeight;
+
+	// Update camera
+	camera.aspect = sizes.width / sizes.height;
+	camera.updateProjectionMatrix();
+
+	// Update renderer
+	renderer.setSize(sizes.width, sizes.height);
+	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+	75,
+	sizes.width / sizes.height,
+	0.1,
+	100,
+);
+camera.position.set(-3, 3, 3);
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+	canvas: canvas,
+});
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+let oldElapsedTime = 0;
+const tick = () => {
+	const elapsedTime = clock.getElapsedTime();
+	const deltaTime = elapsedTime - oldElapsedTime;
+	oldElapsedTime = elapsedTime;
+
+	// apply wind force
+	sphereBody.applyForce(new CANNON.Vec3(-0.5, 0, 0), sphereBody.position);
+
+	world.step(1 / 60, deltaTime, 3);
+	sphere.position.copy(sphereBody.position);
+
+	controls.update();
+	renderer.render(scene, camera);
+	window.requestAnimationFrame(tick);
+};
+
+tick();
